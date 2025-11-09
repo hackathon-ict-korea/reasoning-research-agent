a5c26e6870bd0c1a7ef77471150f30dde37e161e
Dodolist <dodolist3@gmail.com>
2025-11-08 21:18:54 -0800

    fix: type import

--- lib/prompts/index.ts
+++ lib/prompts/index.ts
@@ -5,11 +5,9 @@ import { getSynthesizerPrompt } from "./personas/synthesizer";
 import {
   getResearcherPrompt,
   listResearcherPersonas,
-  type ResearcherId,
 } from "./personas/researchers";
 
 export type AgentType = "reviewer" | "synthesizer" | "researcher";
 
 export { getReviewerPrompt, getSynthesizerPrompt, getResearcherPrompt };
 export { listResearcherPersonas };
-export type { ResearcherId };
